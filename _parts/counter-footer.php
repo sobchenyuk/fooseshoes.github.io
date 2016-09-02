5c4fa1c396f325cef0904b5da0625787f6dddda5
Andrey Sobchenyuk <ablehands@ukr.net>
2016-09-02 18:17:46 +0300

    first commit

---
+++ _parts/counter-footer.php
@@ -0,0 +1,4 @@
+<!-- Latest compiled and minified JavaScript -->
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
