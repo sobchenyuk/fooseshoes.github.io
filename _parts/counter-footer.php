0299f62db24d5bf2f296c46d0733ebcb6931ff63
Andrey Sobchenyuk <ablehands@ukr.net>
2016-10-04 13:32:37 +0300

    Новое изменение 04,10,2016

--- _parts/counter-footer.php
+++
@@ -1,4 +0,0 @@
-<!-- Latest compiled and minified JavaScript -->
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
--- _parts/counter-header.php
+++
@@ -1,5 +0,0 @@
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css">
-
-<link rel="stylesheet" href="_css/master.css">
